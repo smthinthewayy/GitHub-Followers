08f77cea4c7a5633c10a3b18e856fe1d24b0b612
DanilaWhite <60770374+smthinthewayy@users.noreply.github.com>
2023-05-28 14:23:32 +0300

    Inicial Project Setup

--- GHFollowers/FavoritesListVC.swift
+++ GHFollowers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GHFollowers
 //
 //  Created by Danila Belyi on 28.05.2023.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class FavoritesListVC: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemRed
   }
-
-
 }
-
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,15 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Danila Belyi on 28.05.2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBlue
+  }
+}
