92968d7cf65231bfaea6c22a854f3465cbcdf967
DanilaWhite <60770374+smthinthewayy@users.noreply.github.com>
2023-05-29 16:28:43 +0300

    🎨 style(SceneDelegate.swift): add configureNavigationBar() function to set the tint color of the navigation bar The configureNavigationBar() function sets the tint color of the navigation bar to systemGreen. This improves the visual consistency of the app and makes it easier to identify the navigation bar.

--- GHFollowers/SceneDelegate.swift
+++ GHFollowers/SceneDelegate.swift
@@ -21,6 +21,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     window?.windowScene = windowScene
     window?.rootViewController = createTabBar()
     window?.makeKeyAndVisible()
+
+    configureNavigationBar()
   }
 
   func createSearchNC() -> UINavigationController {
@@ -50,6 +52,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ]
 
     return tabBar
+  }
+
+  func configureNavigationBar() {
+    UINavigationBar.appearance().tintColor = .systemGreen
   }
 
   func sceneDidDisconnect(_ scene: UIScene) {}
