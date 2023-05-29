1edcca133b44c7a372f9b0c58ba3f2f1c57b0853
DanilaWhite <60770374+smthinthewayy@users.noreply.github.com>
2023-05-29 12:20:42 +0300

    ✨ feat(GFTextField.swift): create GFTextField subclass of UITextField with custom styling and placeholder text

---
+++ GHFollowers/GFTextField.swift
@@ -0,0 +1,38 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by Danila Belyi on 29.05.2023.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+  // MARK: - Lifecycle
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Private
+
+  private func configure() {
+    translatesAutoresizingMaskIntoConstraints = false
+    layer.cornerRadius = 10
+    layer.borderWidth = 2
+    layer.borderColor = UIColor.systemGray4.cgColor
+    textColor = .label
+    tintColor = .label
+    textAlignment = .center
+    font = UIFont.preferredFont(forTextStyle: .title2)
+    adjustsFontSizeToFitWidth = true
+    minimumFontSize = 12
+    backgroundColor = .tertiarySystemBackground
+    autocorrectionType = .no
+    placeholder = "Enter a username"
+  }
+}
